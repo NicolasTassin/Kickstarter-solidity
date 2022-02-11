6307ed90312566b6aac1133c1637cf5ab031d26c
NicolasTassin <54062897+NicolasTassin@users.noreply.github.com>
2022-02-11 15:09:46 +0700

    Adding getStaticProps
    semantic-ui-react

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,21 @@
 import React, { useEffect, useState } from "react";
 import instance from "../ethereum/factory.js";
 
-export default function IndexCampaign() {
-  const [campaigns, setCampaigns] = useState([]);
+export default function IndexCampaign({ campaigns }) {
   const [description, setDescription] = useState("");
   const [value, setValue] = useState("");
   const [recipient, setRecipient] = useState("");
 
-  useEffect(async () => {
-    const campaigns = await instance.methods.getDeployedCampaings().call();
-    setCampaigns(campaigns);
-  }, []);
-
-  
   return (
     <>
       <div>{campaigns}</div>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const campaigns = await instance.methods.getDeployedCampaings().call();
+  return {
+    props: { campaigns }, // will be passed to the page component as props
+  };
 }
