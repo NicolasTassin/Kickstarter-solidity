9c7e1c2cf0657b814bba2f3fb5ff84eb77c10c39
NicolasTassin <54062897+NicolasTassin@users.noreply.github.com>
2022-02-11 14:23:57 +0700

    Clean code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,23 +2,15 @@
 import React, { useEffect, useState } from "react";
 import instance from "../ethereum/factory.js";
 
-export default function NewCampaign() {
+export default function IndexCampaign() {
   const [campaigns, setCampaigns] = useState([]);
   const [description, setDescription] = useState("");
   const [value, setValue] = useState("");
   const [recipient, setRecipient] = useState("");
 
-  const [web3, setWeb3] = useState(null);
-  const [address, setAddress] = useState(null);
-  const [contract, setContract] = useState(null);
-  const [totalSupply, setTotalSupply] = useState(0);
-
   useEffect(async () => {
-
     const campaigns = await instance.methods.getDeployedCampaings().call();
     setCampaigns(campaigns);
-    console.log(campaigns, "campaigns");
-
   }, []);
 
   
--- web3.js
+++ web3.js
@@ -6,13 +6,11 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
   // We are in the browser and metamask is running.
   window.ethereum.request({ method: "eth_requestAccounts" });
   web3 = new Web3(window.ethereum);
-  console.log("web3 1");
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
     "https://rinkeby.infura.io/v3/a1c48696c83f469c838298470030eed1"
   );
-  console.log("web3 1");
   web3 = new Web3(provider);
 }
 
