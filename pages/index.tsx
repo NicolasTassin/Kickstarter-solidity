2566c5166289fb08b423c6959cd5ba0db59e58e1
NicolasTassin <54062897+NicolasTassin@users.noreply.github.com>
2022-02-12 09:32:28 +0700

    Styling semantic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,36 @@
 //@ts-nocheck
 import React, { useEffect, useState } from "react";
 import instance from "../ethereum/factory.js";
-
+import { StyledButton } from "./index.styles.ts";
+import { Card } from "semantic-ui-react";
 export default function IndexCampaign({ campaigns }) {
   const [description, setDescription] = useState("");
   const [value, setValue] = useState("");
   const [recipient, setRecipient] = useState("");
 
+  const renderCampaigns = () => {
+    const items = campaigns.map((address) => {
+      return {
+        header: address,
+        description: "view Campaign",
+        fluid: true,
+      };
+    });
+    return <Card.Group items={items} />;
+  };
+
   return (
     <>
-      <div>{campaigns}</div>
+     
+      <div>
+      <link
+    async
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+  />
+  
+        {renderCampaigns()}
+        </div>
     </>
   );
 }
