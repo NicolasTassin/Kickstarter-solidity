3d938399aa2236b03fb5702c6306be110820aab2
NicolasTassin <54062897+NicolasTassin@users.noreply.github.com>
2022-02-14 06:49:30 +0700

    Update requests.tsx

--- pages/campaigns/[address]/requests.tsx
+++ pages/campaigns/[address]/requests.tsx
@@ -3,10 +3,14 @@ import Layout from "../../../components/Layout";
 import { Button } from "semantic-ui-react";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Campain from "../../../ethereum/campaign";
 
-const RequestIndex = () => {
+const RequestIndex = async () => {
   const router = useRouter();
   const { address } = router.query;
+
+  const campaign=Campain(address);
+  const requestCount = await campaign.method.getRequestsCount().call();
   return (
     <Layout>
       <div>RequestIndex</div>
